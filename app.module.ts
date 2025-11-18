097c6d3e8da25253b53efb6415f2857a15c39abd
AhmedHebesha <160162031+AhmedHebesha@users.noreply.github.com>
2025-11-18 23:09:57 +0200

    changes

--- app.module.ts
+++ app.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { OffboardingTask, OffboardingTaskSchema } from './schema/OffBoarding/OffBoardingTask.schema';
-import { OffboardingTracker, OffboardingTrackerSchema } from './schema/OffBoarding/OffBoardingTracker.schema';
-import { TerminationRequest, TerminationRequestSchema } from './schema/OffBoarding/TerminationRequest.schema';
-import { ResignationRequest, ResignationRequestSchema } from './schema/OffBoarding/ResignationRequest.schema';
+
+
+
+
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: OffboardingTask.name, schema: OffboardingTaskSchema },
-      { name: OffboardingTracker.name, schema: OffboardingTrackerSchema },
-      { name: TerminationRequest.name, schema: TerminationRequestSchema },
-      { name: ResignationRequest.name, schema: ResignationRequestSchema },
-    ]),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

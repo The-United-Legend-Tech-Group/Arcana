ab01de6c60903d066749ecf7e9e9d3c4ac4a1409
Amr <kamr08985@gmail.com>
2025-11-18 21:46:39 +0200

    added to the app module the classes and its schema for OffBoarding

--- app.module.ts
+++ app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { OffboardingTask, OffboardingTaskSchema } from './schema/OffBoarding/OffBoardingTask.schema';
+import { OffboardingTracker, OffboardingTrackerSchema } from './schema/OffBoarding/OffBoardingTracker.schema';
+import { TerminationRequest, TerminationRequestSchema } from './schema/OffBoarding/TerminationRequest.schema';
+import { ResignationRequest, ResignationRequestSchema } from './schema/OffBoarding/ResignationRequest.schema';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      { name: OffboardingTask.name, schema: OffboardingTaskSchema },
+      { name: OffboardingTracker.name, schema: OffboardingTrackerSchema },
+      { name: TerminationRequest.name, schema: TerminationRequestSchema },
+      { name: ResignationRequest.name, schema: ResignationRequestSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
